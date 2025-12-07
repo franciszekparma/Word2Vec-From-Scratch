4bea1be3b4fb0f07433ef3bfd1439c838bb20eb1
franciszekparma <Franciszek.parma@gmail.com>
2025-12-07 12:52:40 +0100

    Made important changes

--- Word2Vec/utils.py
+++ Word2Vec/utils.py
@@ -1,4 +1,5 @@
 import torch
+import random
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 SHOW_DATA_STATS = False
@@ -6,5 +7,21 @@ EMB_DIM = 32
 WINDOW = 2
 
 
-def get_random_word(vocab, n, context_words, center_word):
+def sample_negatives(vocab, n, context_words, center_word):
+  neg_vocab = vocab.copy()
+  
+  context_words = list(set(context_words))
+  
+  neg_vocab.pop(center_word, None)
+  for word in context_words:
+    neg_vocab.pop(word, None)
+  
+  vocab_keys = list(neg_vocab.keys())
+  vocab_len = len(vocab_keys)
+  
+  negatives = []
+  for i in range(n):
+    negatives.append(vocab_keys[random.randint(0, vocab_len - 1)])
+    
+  return negatives
   
